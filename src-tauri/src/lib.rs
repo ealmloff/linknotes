737c30e08757806b3a0308e97a4695afae508dee
Evan Almloff <evanalmloff@gmail.com>
2024-10-24 15:18:33 -0500

    implement remaining backend methods

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,13 @@
+use kalosm::language::*;
+use parking_lot::{MappedRwLockReadGuard, RwLock, RwLockReadGuard};
+use serde::{Deserialize, Serialize};
+use slab::Slab;
+use std::ops::Range;
+use std::path::PathBuf;
 use std::{
     num::NonZero,
     sync::{Arc, OnceLock},
 };
-
-use kalosm::language::*;
-use serde::{Deserialize, Serialize};
-use std::ops::Range;
-use std::path::PathBuf;
 use surrealdb::{
     engine::local::{Db, RocksDb},
     sql::Id,
@@ -34,51 +35,122 @@ async fn bert() -> anyhow::Result<&'static Arc<CachedEmbeddingModel<Bert>>> {
 type ContextualDocumentTable =
     DocumentTable<Db, ContextualDocument, Arc<CachedEmbeddingModel<Bert>>, DefaultSentenceChunker>;
 
-static DOCUMENT_TABLE: OnceLock<anyhow::Result<ContextualDocumentTable>> = OnceLock::new();
-static DOCUMENT_TABLE_LOCK: tokio::sync::Mutex<()> = tokio::sync::Mutex::const_new(());
+struct Workspace {
+    location: PathBuf,
+    table: OnceLock<anyhow::Result<ContextualDocumentTable>>,
+    lock: tokio::sync::Mutex<()>,
+}
 
-static LOCATION: &str = "./.braindex";
-
-async fn document_table() -> anyhow::Result<
-    &'static DocumentTable<
-        Db,
-        ContextualDocument,
-        Arc<CachedEmbeddingModel<Bert>>,
-        DefaultSentenceChunker,
-    >,
-> {
-    let _guard = DOCUMENT_TABLE_LOCK.lock().await;
-    if DOCUMENT_TABLE.get().is_none() {
-        let init = || async {
-            let root = PathBuf::from(LOCATION);
-            if !root.exists() {
-                std::fs::create_dir_all(&root)?;
-            }
-            // Create database connection
-            let db = Surreal::new::<RocksDb>(root.join("notes.db")).await?;
-
-            // Select a specific namespace / database
-            db.use_ns("search").use_db("documents").await?;
-
-            // Create a table in the surreal database to store the embeddings
-            let document_table = db
-                .document_table_builder("documents")
-                .at(root.join("documents"))
-                .with_embedding_model(bert().await?.clone())
-                .with_chunker(DefaultSentenceChunker)
-                .build::<ContextualDocument>()
-                .await?;
-
-            anyhow::Ok(document_table)
-        };
-
-        _ = DOCUMENT_TABLE.set(init().await);
+impl Workspace {
+    fn new(location: PathBuf) -> Self {
+        Self {
+            location,
+            table: OnceLock::new(),
+            lock: tokio::sync::Mutex::const_new(()),
+        }
     }
-    DOCUMENT_TABLE
-        .get()
-        .unwrap()
-        .as_ref()
-        .map_err(|err| anyhow::anyhow!(err))
+
+    async fn files(&self) -> anyhow::Result<Vec<PathBuf>> {
+        #[derive(Serialize, Deserialize)]
+        struct FilePath {
+            path: PathBuf,
+        }
+
+        let table = self.document_table().await?;
+        let paths: Vec<FilePath> = table
+            .table()
+            .db()
+            .query("SELECT path FROM documents")
+            .await?
+            .take(0)?;
+        Ok(paths.into_iter().map(|p| p.path).collect())
+    }
+
+    async fn document_table(
+        &self,
+    ) -> anyhow::Result<
+        &DocumentTable<
+            Db,
+            ContextualDocument,
+            Arc<CachedEmbeddingModel<Bert>>,
+            DefaultSentenceChunker,
+        >,
+    > {
+        let _guard = self.lock.lock().await;
+        if self.table.get().is_none() {
+            let init = || async {
+                let root = PathBuf::from(&self.location);
+                if !root.exists() {
+                    std::fs::create_dir_all(&root)?;
+                }
+                // Create database connection
+                let db = Surreal::new::<RocksDb>(root.join("notes.db")).await?;
+
+                // Select a specific namespace / database
+                db.use_ns("search").use_db("documents").await?;
+
+                // Create a table in the surreal database to store the embeddings
+                let document_table = db
+                    .document_table_builder("documents")
+                    .at(root.join("documents"))
+                    .with_embedding_model(bert().await?.clone())
+                    .with_chunker(DefaultSentenceChunker)
+                    .build::<ContextualDocument>()
+                    .await?;
+
+                anyhow::Ok(document_table)
+            };
+
+            _ = self.table.set(init().await);
+        }
+        self.table.get().unwrap().as_ref().map_err(|err| {
+            let err = err.to_string();
+            anyhow::anyhow!(err)
+        })
+    }
+}
+
+// An in memory workspace. This is an id to a global table of notes.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct WorkspaceId {
+    id: usize,
+}
+
+/// This is the in memory list of open workspaces loaded by the frontend. Because we are moving between JS and Rust,
+/// we need to load and unload the workspaces manually.
+static OPEN_WORKSPACES: OnceLock<RwLock<Slab<Workspace>>> = OnceLock::new();
+
+fn open_workspaces() -> &'static RwLock<Slab<Workspace>> {
+    OPEN_WORKSPACES.get_or_init(|| RwLock::new(Slab::new()))
+}
+
+/// Get a reference to a workspace by the id
+fn get_workspace_ref(id: WorkspaceId) -> MappedRwLockReadGuard<'static, Workspace> {
+    RwLockReadGuard::map(open_workspaces().read(), |slab| slab.get(id.id).unwrap())
+}
+
+/// Load a workspace at a path into memory. This will either load the existing workspace from the filesystem or create a new workspace at the path.
+#[tauri::command]
+fn load_workspace(path: PathBuf) -> WorkspaceId {
+    let mut workspaces = open_workspaces().write();
+    let new_workspace = Workspace::new(path);
+    let id = workspaces.insert(new_workspace);
+    WorkspaceId { id }
+}
+
+/// Unload a workspace from memory. This should be called whenever the workspace is closed.
+#[tauri::command]
+fn unload_workspace(id: WorkspaceId) {
+    let mut workspaces = open_workspaces().write();
+    workspaces.remove(id.id);
+}
+
+/// Permanently delete a workspace from the filesystem.
+#[tauri::command]
+fn delete_workspace(id: WorkspaceId) {
+    let workspace = get_workspace_ref(id);
+    let path = workspace.location.clone();
+    _ = std::fs::remove_dir_all(path);
 }
 
 #[derive(Serialize, Deserialize)]
@@ -109,8 +181,9 @@ struct Segment {
 
 /// Set the note with a title, contents and path. The path should be canonicalized so it is consistent regardless of the working directory.
 #[tauri::command]
-async fn add_note(title: String, text: String, path: PathBuf) {
-    let document_table = document_table().await.unwrap();
+async fn add_note(title: String, text: String, path: PathBuf, workspace_id: WorkspaceId) {
+    let workspace = get_workspace_ref(workspace_id);
+    let document_table = workspace.document_table().await.unwrap();
     let db = document_table.table().db();
     let path_string = path.display().to_string();
     let document = Document::from_parts(title, text);
@@ -182,8 +255,9 @@ struct SearchResult {
 
 /// Search for some text in the notes. Returns a list of results with the distance, path and character range of each result.
 #[tauri::command]
-async fn search(text: String, results: usize) -> Vec<SearchResult> {
-    let document_table = document_table().await.unwrap();
+async fn search(text: String, results: usize, workspace_id: WorkspaceId) -> Vec<SearchResult> {
+    let workspace = get_workspace_ref(workspace_id);
+    let document_table = workspace.document_table().await.unwrap();
     let bert = bert().await.unwrap();
     let embedding = bert.embed(text).await.unwrap();
     let nearest = document_table
@@ -214,8 +288,9 @@ async fn search(text: String, results: usize) -> Vec<SearchResult> {
 
 /// Remove a note from a specific path. The path should be canonicalized so it is consistent regardless of the working directory.
 #[tauri::command]
-async fn remove_note(path: PathBuf) {
-    let document_table = document_table().await.unwrap();
+async fn remove_note(path: PathBuf, workspace_id: WorkspaceId) {
+    let workspace = get_workspace_ref(workspace_id);
+    let document_table = workspace.document_table().await.unwrap();
     let db = document_table.table().db();
     let path_string = path.display().to_string();
     // First check if the document already exists
@@ -232,43 +307,75 @@ async fn remove_note(path: PathBuf) {
     }
 }
 
+/// Remove a note from a specific path. The path should be canonicalized so it is consistent regardless of the working directory.
+#[tauri::command]
+async fn read_file(path: PathBuf) -> String {
+    std::fs::read_to_string(path).unwrap()
+}
+
+/// Remove a note from a specific path. The path should be canonicalized so it is consistent regardless of the working directory.
+#[tauri::command]
+async fn files_in_workspace(workspace_id: WorkspaceId) -> Vec<PathBuf> {
+    let workspace = get_workspace_ref(workspace_id);
+    workspace.files().await.unwrap()
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
-        .invoke_handler(tauri::generate_handler![add_note, remove_note, search])
+        .invoke_handler(tauri::generate_handler![
+            add_note,
+            remove_note,
+            search,
+            read_file,
+            files_in_workspace,
+            load_workspace,
+            unload_workspace,
+            delete_workspace
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tokio::test]
 async fn test_notes() {
-    // remove the index
-    std::fs::remove_dir_all(LOCATION).ok();
+    let workspace = load_workspace(PathBuf::from("./testing-workspace"));
+    delete_workspace(workspace);
 
     add_note(
         "my-note".to_string(),
         "my note is here".to_string(),
         PathBuf::from("./testing-remote-note.txt"),
+        workspace,
     )
     .await;
     add_note(
         "my-note".to_string(),
         "my note has changed".to_string(),
         PathBuf::from("./testing-remote-note.txt"),
+        workspace,
     )
     .await;
-    remove_note(PathBuf::from("./testing-remote-note.txt")).await;
+    remove_note(PathBuf::from("./testing-remote-note.txt"), workspace).await;
 
     add_note(
         "search-note".to_string(),
         "my note is here".to_string(),
         PathBuf::from("./testing-search-note.txt"),
+        workspace,
     )
     .await;
-    let results = search("my note is here".to_string(), 10).await;
+    let results = search("my note is here".to_string(), 10, workspace).await;
     assert_eq!(results.len(), 1);
     assert_eq!(results[0].path, "./testing-search-note.txt");
     assert_eq!(results[0].character_range, 0..15);
+
+    let notes = files_in_workspace(workspace).await;
+    assert_eq!(notes.len(), 1);
+    assert_eq!(notes, vec![PathBuf::from("./testing-search-note.txt")]);
+
+    delete_workspace(workspace);
+    unload_workspace(workspace);
 }
